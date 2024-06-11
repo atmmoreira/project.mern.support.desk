d7f38934869623ed95ee2162baf95fc3decf3eb1
Anderson Martins <atmmoreira.rj@gmail.com>
2024-06-10 21:41:00 -0300

    feat: add layout for frontend: home, login and register page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/ReactToastify.css'
+// Components
+import Header from "./components/Header";
+// Pages
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
   return (
     <>
-      <h1>Funcionando</h1>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
     </>
   );
 }
