415704e007164f93e90f283908bd1c60d36dcaf1
Anderson Martins <atmmoreira.rj@gmail.com>
2024-07-26 18:16:43 -0300

    feature: create and get tickets

--- api/server.js
+++ api/server.js
@@ -24,6 +24,7 @@ app.get("/", (req, res) => {
 
 // Users Routes
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
 
 app.use(errorHandler);
 
