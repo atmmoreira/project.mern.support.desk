9c6222d971607de2ccd622fd8d7e96dc02b0125c
Anderson Martins <atmmoreira.rj@gmail.com>
2024-08-07 15:47:15 -0300

    features: update and delete tickets

--- api/controllers/ticketController.js
+++ api/controllers/ticketController.js
@@ -79,8 +79,70 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
+/** *
+ * @description Delete ticket
+ * @route       DELETE /api/tickets/:id
+ * @access      Private
+ */
+const deleteTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  await Ticket.findByIdAndDelete(req.params.id);
+  res.status(200).json({ success: true });
+});
+
+/** *
+ * @description Update ticket
+ * @route       PUT /api/tickets/:id
+ * @access      Private
+ */
+const updateTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  const updateTicket = await Ticket.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updateTicket);
+});
+
 module.exports = {
   getTickets,
   getTicket,
   createTicket,
+  deleteTicket,
+  updateTicket,
 };
--- api/routes/ticketRoutes.js
+++ api/routes/ticketRoutes.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const {
-  createTicket,
   getTickets,
   getTicket,
+  createTicket,
+  updateTicket,
+  deleteTicket,
 } = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
-router.route("/:id").get(protect, getTicket);
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .put(protect, updateTicket)
+  .delete(protect, deleteTicket);
 
 module.exports = router;
