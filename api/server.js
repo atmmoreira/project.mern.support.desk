7396f89cf03edccac38d610235e5340c880fa157
Anderson Martins <atmmoreira.rj@gmail.com>
2024-09-04 17:45:09 -0300

    goba to port 5000

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ const dotenv = require("dotenv").config();
 connectDB();
 
 // Variables
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 // Instanciate Express
 const app = express();
